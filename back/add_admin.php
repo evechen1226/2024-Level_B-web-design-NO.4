a1f62e0ca14dde6be6750ab2739ebd02e93bfc08
evechen1226 <eveliving2021@gmail.com>
2024-01-31 09:00:12 +0800

    新增後台管理頁面及管理頁面表單

---
+++ back/add_admin.php
@@ -0,0 +1,39 @@
+<h2 class="ct">
+    新增管理帳號
+</h2>
+<table class="all">
+    <tr>
+        <td class="tt ct">帳號</td>
+        <td class="pp"><input type="text" name="acc" id="acc"></td>
+    </tr>
+    <tr>
+        <td class="tt ct">密碼</td>
+        <td class="pp"><input type="text" name="pw" id="pw"></td>
+    </tr>
+    <tr>
+        <td class="tt ct">權限</td>
+        <td class="pp">
+            <div>
+                <input type="checkbox" name="pr[]" vlaue="1">
+                商類分類管理
+            </div>
+            <div>
+                <input type="checkbox" name="pr[]" value="2">
+                訂單管理
+            </div>
+            <div>
+                <input type="checkbox" name="pr[]" value="3">
+                會員管理
+            </div>
+            <div>
+                <input type="checkbox" name="pr[]" value="4">
+                頁尾版權區管理
+            </div>
+            <div>
+                <input type="checkbox" name="pr[]" value="5">
+                最新消息管
+            </div>
+
+        </td>
+    </tr>
+</table>
---
+++ back/admin.php
@@ -0,0 +1,3 @@
+<dic class="ct">
+    <button onclick="location.href='?do=add_admin'">新增管理員</button>
+</dic>
