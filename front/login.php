7c7c66dc501a334bcab02e2a0a0b503abe79ffd5
evechen1226 <eveliving2021@gmail.com>
2024-04-19 20:07:39 +0800

    front/login.php--製作登入畫面

---
+++ front/login.php
@@ -0,0 +1,21 @@
+<h1>第一次購買</h1>
+<img src="../img/0413.jpg" alt="location.href='?do=reg'">
+<h1>會員登入</h1>
+<table>
+  <tr>
+    <td class="tt">帳號</td>
+    <td class="pp"><input type="text" name="acc" id="acc"></td>
+  </tr>
+  <tr>
+    <td class="tt">密碼</td>
+    <td class="pp"><input type="text" name="pw" id="pw"></td>
+  </tr>
+  <tr>
+    <td class="tt">驗證碼</td>
+    <td class="pp"><input type="text" name="ans" id="ans"></td>
+  </tr>
+</table>
+
+<div class="ct">
+  <button onclick="login('mem')">確認</button>
+</div>
