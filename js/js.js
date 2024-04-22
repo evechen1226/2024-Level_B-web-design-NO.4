5ad111354efe881e02edfbc95d60333ad832ffed
evechen1226 <eveliving2021@gmail.com>
2024-04-22 23:47:14 +0800

    feat:js/js.js--增加登入驗證程式login()

--- js/js.js
+++ js/js.js
@@ -1,12 +1,29 @@
 // JavaScript Document
-function lof(x)
-{
-	location.href=x
+function lof(x) {
+	location.href = x
 }
 
 // 刪除功能
-function del(table,id){
-	$.post("./api/del.php",{table,id},()=>{
+function del(table, id) {
+	$.post("./api/del.php", { table, id }, () => {
 		location.reload()
+	})
+}
+
+// 登入驗證功能
+
+function login() {
+	$.get("./api/chk_ans.php", { ans: $("#ans").val() }, (chk) => {
+		if (parseInt(chk) == 0) {
+			alert("驗證碼錯誤，請重新輸入")
+		} else {
+			$.pos("./api/login.php", { table, acc: $("#acc").val, pw: $("#pw").val() }, (res) => {
+				if (parent(res) == 0) {
+					alert("帳號或密碼錯誤，請重新輸入")
+				} else {
+					location.href = 'index.php';
+				}
+			})
+		}
 	})
 }
