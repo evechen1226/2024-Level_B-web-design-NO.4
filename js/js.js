d88165df8745e49b45fad66000ca2ded2fc1ca1e
evechen1226 <eveliving2021@gmail.com>
2024-04-24 02:48:18 +0800

    feat:front/admin.php--新增管理員登入頁面

--- js/js.js
+++ js/js.js
@@ -11,14 +11,13 @@ function del(table, id) {
 }
 
 // 登入驗證功能
-
-function login() {
+function login(table) {
 	$.get("./api/chk_ans.php", { ans: $("#ans").val() }, (chk) => {
 		if (parseInt(chk) == 0) {
 			alert("驗證碼錯誤，請重新輸入")
 		} else {
-			$.pos("./api/chk_pw.php", { table, acc: $("#acc").val(), pw: $("#pw").val() }, (res) => {
-				if (parent(res) == 0) {
+			$.post("./api/chk_pw.php", { table, acc: $("#acc").val(), pw: $("#pw").val() }, (res) => {
+				if (parseInt(res) == 0) {
 					alert("帳號或密碼錯誤，請重新輸入")
 				} else {
 					location.href = 'index.php';
@@ -27,3 +26,4 @@ function login() {
 		}
 	})
 }
+
