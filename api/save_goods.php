4284b3307d5b5ccc670738ca02e5f12a465d2f33
evechen1226 <eveliving2021@gmail.com>
2024-05-03 06:44:13 +0800

    fix:api/save_goods.php--修正['imp_name']為['tmp_name']

--- api/save_goods.php
+++ api/save_goods.php
@@ -1,7 +1,7 @@
 <?php include_once 'db.php';
 if(!empty($_FILES['img']['tmp_name'])){
     $_POST['img']=$_FILES['img']['name'];
-    move_uploaded_file($_FILES['img']['imgp_name'],"../img/{$_FILES['img']['name']}");
+    move_uploaded_file($_FILES['img']['tmp_name'],"../img/{$_FILES['img']['name']}");
 
 }
 $_POST['no']=rand(100000,99999);
